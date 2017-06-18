ea4144af62924f0b59830f14c9ec498036980bdc
Alleo Indong <freedom_war550@live.com.ph>
2017-06-19 00:56:52 +0800

    event selection

--- app/src/main/java/com/cloudwalkdigital/verifynegotiator/EventSelectionActivity.java
+++ app/src/main/java/com/cloudwalkdigital/verifynegotiator/EventSelectionActivity.java
@@ -1,7 +1,9 @@
 package com.cloudwalkdigital.verifynegotiator;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class EventSelectionActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class EventSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_selection);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
