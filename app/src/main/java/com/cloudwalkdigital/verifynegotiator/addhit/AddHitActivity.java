5143484f95d20713d89937064eb81f6c2227f99a
Alleo Indong <freedom_war550@live.com.ph>
2017-06-20 13:41:49 +0800

    added spinner

--- app/src/main/java/com/cloudwalkdigital/verifynegotiator/addhit/AddHitActivity.java
+++ app/src/main/java/com/cloudwalkdigital/verifynegotiator/addhit/AddHitActivity.java
@@ -9,6 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.cloudwalkdigital.verifynegotiator.R;
 import com.cloudwalkdigital.verifynegotiator.events.EventSelectionActivity;
@@ -16,6 +19,7 @@ import com.cloudwalkdigital.verifynegotiator.utils.SessionManager;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AddHitActivity extends AppCompatActivity {
@@ -23,6 +27,15 @@ public class AddHitActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
 
     private final String TAG = "ADDHITACTIVITY";
+
+    @BindView(R.id.inputName) EditText mName;
+    @BindView(R.id.inputEmail) EditText mEmail;
+    @BindView(R.id.inputContactNo) EditText mContactNo;
+    @BindView(R.id.inputSchoolName) EditText mSchoolName;
+    @BindView(R.id.inputDesignation) EditText mDesignation;
+    @BindView(R.id.inputAddress) EditText mAddress;
+    @BindView(R.id.inputOtherDetails) EditText mOtherDetails;
+    @BindView(R.id.spinnerLocation) Spinner mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,13 @@ public class AddHitActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setupToolbar();
+
+        setupSpinner();
+    }
+
+    private void setupSpinner() {
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.location_arrays, R.layout.spinner_item);
+        mLocation.setAdapter(adapter);
     }
 
     private void setupToolbar() {
--- app/src/main/java/com/cloudwalkdigital/verifynegotiator/data/models/Hit.java
+++ app/src/main/java/com/cloudwalkdigital/verifynegotiator/data/models/Hit.java
@@ -1,8 +1,111 @@
 package com.cloudwalkdigital.verifynegotiator.data.models;
 
+import com.google.gson.annotations.SerializedName;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
 /**
  * Created by alleoindong on 6/19/17.
  */
 
-public class Hit {
+public class Hit extends RealmObject {
+    @PrimaryKey
+    private Integer id;
+
+    @SerializedName("project_id")
+    private Integer projectId;
+
+    @SerializedName("hit_timestamp")
+    private String hitTimestamp;
+
+    private String name;
+
+    @SerializedName("school_name")
+    private String schoolName;
+
+    private String email;
+
+    @SerializedName("contact_number")
+    private String contactNumber;
+
+    private String designation;
+
+    private String address;
+
+    @SerializedName("other_details")
+    private String otherDetails;
+
+    private String image;
+
+    private String location;
+
+    public Hit() {
+    }
+
+    public Hit(Integer id, Integer projectId, String hitTimestamp,
+               String name, String schoolName, String email, String contactNumber,
+               String designation, String address, String otherDetails, String image,
+               String location) {
+        this.id = id;
+        this.projectId = projectId;
+        this.hitTimestamp = hitTimestamp;
+        this.name = name;
+        this.schoolName = schoolName;
+        this.email = email;
+        this.contactNumber = contactNumber;
+        this.designation = designation;
+        this.address = address;
+        this.otherDetails = otherDetails;
+        this.image = image;
+        this.location = location;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    public String getHitTimestamp() {
+        return hitTimestamp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getOtherDetails() {
+        return otherDetails;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
